68daeabd6e255110cdd7af77415d0bc96aa2ce4e
winex01 <winnie131212592@gmail.com>
2019-12-07 23:20:08 +0900

    add new menu policy

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,4 @@
-<!--==========================
-  Header
-  ============================-->
+
   <header id="header" class="fixed-top">
     <div class="container">
       @include('layouts.site-logo')
@@ -12,7 +10,8 @@
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#team">Team</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
+          <li><a href="#why-us">Policies & Others</a></li>
+          {{-- <li class="drop-down"><a href="">Drop Down</a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="drop-down"><a href="#">Drop Down 2</a>
@@ -28,7 +27,7 @@
               <li><a href="#">Drop Down 4</a></li>
               <li><a href="#">Drop Down 5</a></li>
             </ul>
-          </li>
+          </li> --}}
           <li><a href="#contact">Contact Us</a></li>
         </ul>
       </nav><!-- .main-nav -->
