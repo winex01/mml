c7ef5a5df8da7f422861b90992ce6b36133c648b
winex01 <winnie131212592@gmail.com>
2019-12-07 21:55:06 +0900

    Services migration, views and model

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -9,67 +9,64 @@
 
       <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">CLADDING INSULATION</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">CLADDING INSULATION</a></h4>
         </div>
       </div>
       <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">DUCTING INSULATION</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">DUCTING INSULATION</a></h4>
         </div>
       </div>
 
       <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">TANK CLEANING</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">TANK CLEANING</a></h4>
         </div>
       </div>
       <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">PAINTING WORKS</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">PAINTING WORKS</a></h4>
         </div>
       </div>
 
       <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">SANDBLASTING</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">SANDBLASTING</a></h4>
         </div>
       </div>
-
       <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">CIVIL WORKS</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">CIVIL WORKS</a></h4>
         </div>
       </div>
 
       <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">MECHANICAL WORKS</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">MECHANICAL WORKS</a></h4>
         </div>
       </div>
-
       <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">ELECTRICAL WORKS</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">ELECTRICAL WORKS</a></h4>
         </div>
       </div>
 
 
       <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">AUXILIARY</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">AUXILIARY</a></h4>
         </div>
       </div>
-
       <div class="col-md-6 col-lg-5 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">INTERIOR SOLUTION</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">INTERIOR SOLUTION</a></h4>
         </div>
       </div>
 
 
       <div class="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp" data-wow-delay="0.2s" data-wow-duration="1.4s">
         <div class="box">
-          <h4 class="title"><a href="">FABRICATION</a></h4>
+          <h4 class="title"><a href="javascript:void(0)">FABRICATION</a></h4>
         </div>
       </div>
 
