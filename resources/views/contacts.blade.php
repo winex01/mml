a5e2df9b34b6a1b0784a3dbcc5a1723297b9df9c
winex01 <winnie131212592@gmail.com>
2019-12-08 20:50:57 +0900

    add google map

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -12,8 +12,11 @@
 
           <div class="col-lg-6">
             <div class="map mb-4 mb-lg-0">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 312px;" allowfullscreen></iframe>
+              {{-- <iframe src="https://maps.google.com/maps?q=Brgy%20San%20Antonio%20Cebu%20city&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0; width: 100%; height: 312px;" allowfullscreen></iframe> --}}
+              {!! setting('site.google_map') !!}
             </div>
+
+
           </div>
 
           <div class="col-lg-6">
